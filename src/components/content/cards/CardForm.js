183f0659621e47b7161e0b5c9c2345071a6d8110
E-Shelton98 <erik.shelton92@gmail.com>
2022-05-10 11:15:23 -0400

    implement checkboxes for card colors

--- src/components/content/cards/CardForm.js
+++ src/components/content/cards/CardForm.js
@@ -7,14 +7,62 @@ function CardForm({ getCards }) {
   const [cardRestricted, setCardRestricted] = useState(false)
 
   //TO SELECT A CARD THAT IS ALL COLORS AT ONCE DO BGRUW
-  const [cardColor, setCardColor] = useState([])
+  const [cardColorBlack, setCardColorBlack] = useState(false)
+  const [cardColorGreen, setCardColorGreen] = useState(false)
+  const [cardColorRed, setCardColorRed] = useState(false)
+  const [cardColorBlue, setCardColorBlue] = useState(false)
+  const [cardColorWhite, setCardColorWhite] = useState(false)
   const [cardCMC, setCardCMC] = useState('')
 
   //SEARCH MUST BE STRICT UPPER CASING OF FIRST LETTER (ie. Zephyr Boots)
   const [cardName, setCardName] = useState()
 
-  async function searchCards(e) {
+  async function searchCards(
+    cardColorBlack,
+    cardColorGreen,
+    cardColorRed,
+    cardColorBlue,
+    cardColorWhite,
+    e
+  ) {
     e.preventDefault()
+
+    let cardColorStringFunction = function (
+      cardColorBlack,
+      cardColorGreen,
+      cardColorRed,
+      cardColorBlue,
+      cardColorWhite
+    ) {
+      let cardColor = []
+      if (cardColorBlack) {
+        cardColor.push('B')
+      }
+      if (cardColorGreen) {
+        cardColor.push('G')
+      }
+      if (cardColorRed) {
+        cardColor.push('R')
+      }
+      if (cardColorBlue) {
+        cardColor.push('U')
+      }
+      if (cardColorWhite) {
+        cardColor.push('W')
+      }
+
+      return cardColor
+    }
+
+    let cardColor = cardColorStringFunction(
+      cardColorBlack,
+      cardColorGreen,
+      cardColorRed,
+      cardColorBlue,
+      cardColorWhite
+    )
+
+    cardColor = cardColor.join('')
 
     try {
       let cardFetchString = `http://localhost:5000/cards?format=${cardLegality}&colors=${cardColor}&cmc=${cardCMC}&name=${cardName}`
@@ -33,10 +81,20 @@ function CardForm({ getCards }) {
 
   return (
     <div>
-      <form onSubmit={searchCards}>
+      <form
+        onSubmit={(e) =>
+          searchCards(
+            cardColorBlack,
+            cardColorGreen,
+            cardColorRed,
+            cardColorBlue,
+            cardColorWhite,
+            e
+          )
+        }>
         <input
           type='text'
-          placeholder='Legal In'
+          placeholder='legal In'
           onChange={(e) => {
             setCardLegality(e.target.value)
           }}
@@ -46,20 +104,49 @@ function CardForm({ getCards }) {
         <input
           type='checkbox'
           id='restricted'
-          name='restricted_modifier'
-          onChange={(e) => {
+          name='restricted-modifier'
+          onChange={() => {
             setCardRestricted(!cardRestricted)
           }}
         />
 
-        <input
-          type='text'
-          placeholder='Card Color'
-          onChange={(e) => {
-            setCardColor(e.target.value)
-          }}
-          value={cardColor}
-        />
+        <div className='color-checkboxes'>
+          <input
+            type='checkbox'
+            id='color-black'
+            name='color'
+            onChange={() => {
+              setCardColorBlack(!cardColorBlack)
+            }}></input>
+          <input
+            type='checkbox'
+            id='color-green'
+            name='color'
+            onChange={() => {
+              setCardColorGreen(!cardColorGreen)
+            }}></input>
+          <input
+            type='checkbox'
+            id='color-red'
+            name='color'
+            onChange={() => {
+              setCardColorRed(!cardColorRed)
+            }}></input>
+          <input
+            type='checkbox'
+            id='color-blue'
+            name='color'
+            onChange={() => {
+              setCardColorBlue(!cardColorBlue)
+            }}></input>
+          <input
+            type='checkbox'
+            id='color-white'
+            name='color'
+            onChange={() => {
+              setCardColorWhite(!cardColorWhite)
+            }}></input>
+        </div>
         <input
           type='text'
           placeholder='Card CMC'
